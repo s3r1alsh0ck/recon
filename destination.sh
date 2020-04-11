bc53304f8e8fe4f5b5b2cf42407953c3570dff2d
jayashankarkumar <62589036+jayashankarkumar@users.noreply.github.com>
2020-04-11 09:34:14 +0530

    Update destination.sh

--- destination.sh
+++ destination.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mv /root/recon/final-domains.txt $1
-mv /root/recon/alive-domains.txt $1
-mv /root/recon/ports-domains.txt.nmap $1
-mv /root/recon/wayback-domains.txt $1
-mv /root/recon/automation/geckodriver.log $1
-mv /root/recon/eyewitness $1
+mv /root/final-domains.txt $1
+mv /root/alive-domains.txt $1
+mv /root/ports-domains.txt.nmap $1
+mv /root/wayback-domains.txt $1
+mv /root/automation/geckodriver.log $1
+mv /root/eyewitness $1
 echo
 echo "Files reached the destenation!!"
 
