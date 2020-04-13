3aba625b36023473157af2ae81f15602b3242e01
jayashankarkumar <62589036+jayashankarkumar@users.noreply.github.com>
2020-04-13 17:30:47 +0530

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -14,7 +14,7 @@ findomain -t $1 -u /root/sub.txt
 echo
 cat /root/sub.txt | grep -Po "(\w+\.\w+\.\w+)$" | sort -u | tee -a /root/third-stage-sub.txt | wc -l
 echo
-subfinder -dL /root/third-stage-sub.txt -o /root/subdomains.txt;done
+for domain in $(cat /root/third-stage-sub.txt); do subfinder -d $domain -o /root/subdomains.txt;done
 echo
 for domain in $(cat /root/third-stage-sub.txt); do findomain -t $domain -u /root/subdomains.txt;done
 echo
@@ -22,6 +22,7 @@ for domain in $(cat /root/third-stage-sub.txt); do curl -s https://certspotter.c
 echo
 for domain in $(cat /root/third-stage-sub.txt); do python /root/tools/Sublist3r/sublist3r.py -d $domain -o /root/subdomains.txt;done
 echo
+cat /root/sub.old >> sub.txt
 cat /root/subdomains.txt | tee -a /root/sub.txt
 echo
 cat /root/sub.txt | sort -u | sed 's/<.*//' |  tee -a /root/target-domains.txt
